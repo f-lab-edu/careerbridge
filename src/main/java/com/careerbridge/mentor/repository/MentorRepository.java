751b0502c79267f6845504dd0a044df67b644c4f
kibeomchoi <kibeom2000@gmail.com>
2026-07-02 18:00:55 +0900

    [#40] bugfix : Mentor-profile merge Error

--- src/main/java/com/careerbridge/mentor/repository/MentorRepository.java
+++ src/main/java/com/careerbridge/mentor/repository/MentorRepository.java
@@ -2,17 +2,13 @@ package com.careerbridge.mentor.repository;
 
 import com.careerbridge.mentor.entity.Mentor;
 import com.careerbridge.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MentorRepository  {
-    List<Mentor> search(Long jobCategoryId, String keyword);
-
     List<Mentor> search(Long jobCategoryId, String keyword);
 
     Optional<Mentor> findByUser(User user);
