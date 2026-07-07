11dff43118de5ac546a446826c4cdb80e1e4c64f
kibeomchoi <kibeom2000@gmail.com>
2026-07-07 15:06:52 +0900

    [#46] feature : ProductController 생성

--- src/main/java/com/careerbridge/product/dto/ProductUpdateRequest.java
+++ src/main/java/com/careerbridge/product/dto/ProductUpdateRequest.java
@@ -2,7 +2,8 @@ package com.careerbridge.product.dto;
 
 import com.careerbridge.product.entity.ProductStatus;
 
-public record ProductUpdateRequest(String title,
+public record ProductUpdateRequest(Long productId,
+                                   String title,
                                    String description,
                                    Integer price,
                                    Integer duration,
